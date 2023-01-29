b743217a041a350c53bbc698fdade0ae7b0b08ab
Sourov003 <81452244+Sourov003@users.noreply.github.com>
2023-01-28 21:50:35 -0500

    Update and rename Index.html to .py

--- .py
+++ .py
@@ -1,77 +1,3 @@
-# import pandas as pd
-# import base64
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import numpy as np 
-# import yfinance as yf
-# import streamlit as st
-
-# st.title('S&P 500')
-
-# st.markdown("""
-# This app retrives the list of the **S&P 500 (from Wikipedia)
-# *   **python libaries:** based64, pandas, streamlit, numpy, matplot
-# *   **Data Source: [Wikipedia](https://www.wikipedia.org/).
-# """)
-
-# st.sidebar.header('User input Features')
-
-
-# #web Scraping  of S&P 500 data
-# #
-# @st.cache
-# def load_data():
-#     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-#     html = pd.read_html(url, header = 0)
-#     df = html[0]
-#     return df
-
-
-# def price_plot(symbol):
-#   df = pd.DataFrame(data[symbol].Close)
-#   df['Date'] = df.index
-#   plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
-#   plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
-#   plt.xticks(rotation=90)
-#   plt.title(symbol, fontweight='bold')
-#   plt.xlabel('Date', fontweight='bold')
-#   plt.ylabel('Closing Price', fontweight='bold')
-#   return st.pyplot()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#THE CODE FROM THE VIDEO
-
 import streamlit as st
 import pandas as pd
 import base64
@@ -102,11 +28,10 @@ def load_data():
 df = load_data()
 sector = df.groupby('GICS Sector')
 
-# Sidebar - Sector selection
+Sidebar - Sector selection
 sorted_sector_unique = sorted( df['GICS Sector'].unique() )
 selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
-
-# Filtering data
+Filtering data
 df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
 
 st.header('Display Companies in Selected Sector')
@@ -154,9 +79,3 @@ if st.button('Show Plots'):
     st.header('Stock Closing Price')
     for i in list(df_selected_sector.Symbol)[:num_company]:
         price_plot(i)
-
-
-
-
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
